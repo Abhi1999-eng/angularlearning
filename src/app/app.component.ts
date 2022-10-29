19d80275ac61053cb0baaa8a74bfcbc22750db0b
Abhishek Chaubey <abhishekchaubey1999@gmail.com>
2022-10-29 11:54:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  // name = "peter";
+  show = true;
+  // fun(name: any, age: any) {
+  //   alert("Name is: "+ name + " and Age is: " + age)
+  // }
+  // displayVal: string = '';
+  // getValue(val: string) {
+  //   console.log(val);
+  //   this.displayVal = val;
+  // }
+  // count = 0
+  // counter(type: string) {
+  //   type == "add"? this.count++:this.count--;
+  // }
+
+  // countern(){
+    // if (this.count == 0) {
+    //   this.count = 0;
+    // }else{
+    //   this.count--;
+    // }
+  // }
+
 }
